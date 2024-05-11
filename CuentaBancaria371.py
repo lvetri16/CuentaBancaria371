794928ef262136d85a773a2898d973f27f351c95
leov266 <leov266@gmail.com>
2024-05-11 12:04:38 -0400

    Quinto commit: Agrego comentarios

--- CuentaBancaria371.py
+++ CuentaBancaria371.py
@@ -1,8 +1,10 @@
+#declaracion de clase cuenta
 class Cuenta:
     def __init__(self):
         self._persona = ""
         self._cantidad = 0
     
+    #definicion de los getter y setter del atributo persona
     @property
     def persona(self):
         return self._persona
@@ -11,6 +13,7 @@ class Cuenta:
     def persona(self, persona):
         self._persona = persona
     
+    #definicion de los getter y setter del atributo cantidad
     @property
     def cantidad(self):
         return self._cantidad
@@ -19,17 +22,21 @@ class Cuenta:
     def cantidad(self, cantidad):
         if cantidad >= 0:
             self._cantidad = cantidad
-            
+    
+    # Definicion del Metodo ingresar cantidad   
     def ingresar_cantidad(self, cantidad):
         if cantidad > 0:
             self._cantidad += cantidad
-    
+            
+    #Definicion del metodo retirar cantidad
     def retirar_cantidad(self, cantidad):
         self._cantidad -= cantidad
-
+    
+    #Definicion del metodo mostrar datos
     def mostrar_datos(self):
         return f"Datos de la cuenta:\nPersona: {self._persona}\nCantidad: {self._cantidad}\n"
 
+#Invoco la clase cuenta
 cuenta = Cuenta()
 cuenta.persona = "Juan"
 cuenta.cantidad = 1000
